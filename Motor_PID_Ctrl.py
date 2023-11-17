72d883bc09ca5d7bb71f832a166156cf7ed376c2
iamjoseph1 <iamjoseph1129@gmail.com>
2023-11-17 07:13:44 +0000

    fupdate

--- Motor_PID_Ctrl.py
+++ Motor_PID_Ctrl.py
@@ -57,11 +57,16 @@ while True:
     ret, thresh1 = cv2.threshold(frame_blur,123,255,cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    frame_hsv = cv2.cvtColor(frame_lr,cv2.COLOR_BGR2HSV)
-    mask_red = cv2.inRange(frame_hsv, (160,128,128), (180,255,255)) #HSV순서-빨간색 검출, S(선명도)를 조작해 살색 검출 제한
+    #frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    #mask_red = cv2.inRange(frame_hsv, (160,100,100), (160,255,255)) #HSV순서-빨간색 검출, S(선명도)를 조작해 살색 검출 제한
     #ㄴinRange함수를 사용한 mask영상은 이진화 영상이다.
     # frame_red = cv2.bitwise_and(frame_lr_shine,frame_lr_shine,mask = mask_red)
     #ㄴ원본 영상과 mask에 대해 and연산을 수행해 1인 곳만(빨간색인 곳만) 살리고 나머지는 0(검은색)으로 표시 
+    
+    img_hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([170,50,50]) #example value
+    upper_red = np.array([180,255,255]) #example value
+    mask_red = cv2.inRange(img_hsv, lower_red, upper_red)
     red_contours, red_ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
 
@@ -90,18 +95,20 @@ while True:
             print('left_vel : %d       right_vel : %d' %(left_vel, right_vel))
             DXL.Dual_MotorController(left_vel, right_vel)
 
-            if len(red_contours) > 0:
+            if len(red_contours) != 0:
                 print('red detected, STOP')
                 DXL.Dual_MotorController(0, 0)
+                time.sleep(0.5)
+                DXL.Dual_MotorController(100, -100)
                 time.sleep(3)
-                DXL.Dual_MotorController(100, 100)
-                break
+                continue
 
         else :
             DXL.Dual_MotorController(0,0)
 
     cv2.imshow('frame_lr', frame_lr) #orignal frame(fliped) + contours + centroid
     cv2.imshow('frame_thresh', thresh1)
+    cv2.imshow('mask_red', mask_red)
 
     # # PID control for velocity
     # if cX > 0:
